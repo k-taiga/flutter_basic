0df7e02f64a8a4843ba94f548edbe5e93e59f426
K-taiga <taigakatou0329@gmail.com>
2025-03-08 17:50:02 +0900

    shared_preferencesを使用して設定を保存および読み込む機能を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,170 +30,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _msg = "";
-  bool _isChecked = false;
-  int _radioValue = 0;
   bool _isOn = false;
+
+  _MyHomePageState() {
+    final f = _loadSettingA();
+    f.then((value) => {
+          setState(() {
+            _isOn = value;
+          })
+        });
+  }
+
+  Future<void> _saveSettingA(bool value) async {
+    // shared_preferencesのインスタンスを取得
+    final prefs = await SharedPreferences.getInstance();
+
+    // shared_preferencesにデータを保存
+    await prefs.setBool(
+        'settingA',
+        value);
+  }
+
+  // shared_preferencesからデータを取得
+  Future<bool> _loadSettingA() async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getBool('settingA');
+    return value ?? false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    const text = Text('Hello, Flutter');
-    const textArea = Column(
-      children: [
-        text,
-      ],
-    );
-
-    final button = ElevatedButton(
-      onPressed: () {
-        print("ボタンが");
-        print("押されました");
-      },
-      child: const Text("ボタン"),
-    );
-
-    fn() {
-      print("関数実行");
-    }
-
-    final textButton = TextButton(
-      onPressed: fn,
-      child: const Text("テキストボタン"),
-    );
-
-    final buttonArea = Row(
-      children: [button, textButton],
-    );
-
-    final textFieldText = Text('メッセージ $_msg');
-    final controller = TextEditingController();
-
-    final textField = TextField(
-      controller: controller,
-      autofocus: true,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: '内容',
-        hintText: '100文字以内で入力してください',
-      ),
-      onChanged: (String text) {
-        print("value: $text");
-      },
-    );
-
-    setMsg() {
-      setState(() {
-        _msg = controller.text;
-      });
-    }
-
-    clear() {
-      controller.clear();
-    }
-
-    final setMsgButton = ElevatedButton(
-      onPressed: setMsg,
-      child: const Text("反映"),
-    );
-
-    final clearButton = ElevatedButton(
-      onPressed: clear,
-      child: const Text("クリア"),
-    );
-
-    final textFieldArea = Column(
-      children: [
-        textFieldText,
-        textField,
-        Row(
-          children: [setMsgButton, clearButton],
-        )
-      ],
-    );
-
-    final isCheckedText = Text("チェック: ${_isChecked ? 'ON' : 'OFF'}");
-
-    final checkBox = Checkbox(
-      value: _isChecked,
-      onChanged: (bool? value) {
-        setState(() {
-          _isChecked = value!;
-        });
-      },
-    );
-
-    const checkBoxText = Text("チェックボックス");
-
-    final checkBoxAre = Row(
-      children: [
-        isCheckedText,
-        checkBoxText,
-        checkBox,
-      ],
-    );
-
-    final radio1 = Radio(
-      value: 1,
-      groupValue: _radioValue,
-      onChanged: (int? value) {
-        setState(() {
-          _radioValue = value!;
-        });
-      },
-    );
-
-    const radio1Text = Text("Android");
-
-    final radio2 = Radio(
-      value: 2,
-      groupValue: _radioValue,
-      onChanged: (int? value) {
-        setState(() {
-          _radioValue = value!;
-        });
-      },
-    );
-
-    const radio2Text = Text("iOS");
-
-    const radioMap = {0: "未選択", 1: "Android", 2: "iOS"};
-    final radioText = Text("デバイス: ${radioMap[_radioValue]}");
-
-    final radioArea = Row(
-      children: [
-        radio1,
-        radio1Text,
-        const SizedBox(width: 10.0),
-        radio2,
-        radio2Text,
-        const SizedBox(width: 20.0),
-        radioText
-      ],
-    );
-
-    final switchText = Text(_isOn ? "ON" : "OFF");
+    final switchText = Text(_isOn ? "設定A:オン" : "設定A:オフ");
     final toggle = Switch(
       value: _isOn,
-      onChanged: (bool value) {
+      onChanged: (value) {
         setState(() {
           _isOn = value;
+          _saveSettingA(value);
         });
       },
-    );
-
-    final switchArea = Row(
-      children: [switchText, toggle],
     );
 
     final body = SafeArea(
-      child: Column(
+      child: Row(
         children: [
-          textArea,
-          buttonArea,
-          textFieldArea,
-          checkBoxAre,
-          radioArea,
-          switchArea,
+          switchText,
+          toggle,
         ],
       ),
     );
